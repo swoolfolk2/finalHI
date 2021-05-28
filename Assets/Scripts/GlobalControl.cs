e8f3610361a4c282697bc85e5b04c98b9f567b51
swoolfolk2 <swoolfolkmar@gmail.com>
2021-05-28 14:14:34 -0600

    corrected code and deleted unused assets

--- Assets/Scripts/GlobalControl.cs
+++ Assets/Scripts/GlobalControl.cs
@@ -6,6 +6,5 @@ using System.Collections.Generic;
 */
 public static class GlobalControl
 {
-    public static List<string> fields = new List<string>();
-    public static int counter = 0;
+    public static List<string> fields = new List<string>(); // fields of the players 
 }
